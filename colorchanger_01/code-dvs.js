697ab0a449c803e32a444379d730b7445e8b36cf
devansh <dsppatel.149@gmail.com>
2024-07-27 22:15:09 +0530

    change

--- colorchanger_01/code-dvs.js
+++ colorchanger_01/code-dvs.js
@@ -7,6 +7,7 @@ buttons.forEach(function (button) {
   button.addEventListener('click', function (e) {
     console.log(e);
     console.log(e.target);
+    
     if (e.target.id === 'grey') {
       body.style.backgroundColor = e.target.id;
     }
